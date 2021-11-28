cdae307f850efb9b7aa9202b42f5ca7a8271e6c9
Lucas Ferrarezi Paim <lucas.paim@defensoria.ba.def.br>
2021-11-27 22:56:56 -0300

    adding graphs and plotting data from API into them

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,12 +15,26 @@
             Primary
         </v-btn>
 
+        <div class="small">
+            <line-chart
+                v-if="show_graph_1"
+                :chart-data="data_1"
+            ></line-chart>
+
+            <BarChart
+                v-if="show_graph_1"
+                :chart-data="data_1"
+            ></BarChart>
+        </div>
     </v-container>
 </template>
 
 
 <script>
     import axios from '../services/axios'
+    import moment from 'moment'
+    import BarChart from './graphs/BarChart'
+    import LineChart  from './graphs/LineChart'
 
     export default
     {
@@ -30,10 +44,19 @@
                 countries_loading: true,
                 countries: [],
                 selected_iso: "",
+                data_1: {},
+                show_graph_1: false,
             }
         },
+
         name: 'Home',
-        components: {},
+
+        components:
+        {
+            BarChart,
+            LineChart,
+        },
+
         methods:
         {
             async getCountries()
@@ -44,7 +67,6 @@
                     const result = await axios.get("/regions")
                     this.countries = result.data.data
                     this.countries_loading = false
-                    //TODO: handle empty data array -- false positive
                 }
                 catch (error)
                 {
@@ -58,7 +80,64 @@
                 const country = this.countries.find((i) => i.iso === value)
                 return country? country.name : "Error"
             },
+
+            async getReports(iso, past_days_range = 3)
+            {
+                this.show_graph_1 = false
+                let color = this.getRandomColor()
+                this.data_1 =
+                {
+                    labels: [],
+                    datasets: [{
+                        label: '# of new cases for selected country',
+                        data: [],
+                        fill: false,
+                        backgroundColor: [color, this.getRandomColor()],
+                        borderColor: color,
+                    }]
+                }
+
+                for (let index = past_days_range; index>0; index--)
+                {
+                    try
+                    {
+                        let date = moment()
+                        date = date.subtract(index, 'days').format("YYYY-MM-DD")
+                        
+                        this.data_1.labels.push(moment(date).format("DD/MM/YYYY"))
+
+                        const result = await axios.get(`
+                            /reports/total?date=${date}&iso=${iso}
+                        `)
+                        
+                        this.data_1.datasets[0].data.push(result.data.data.confirmed_diff)
+                    }
+                    catch (error)
+                    {
+                        console.log("error!")
+                        console.log(error)
+                    }
+                }
+                
+
+                console.log(this.data_1)
+                //TODO: handle empty data array -- false positive
+                //TODO: adicionar flag de erro para nao setar true abaixo caso a flag de erro seja true
+                this.show_graph_1 = true
+            },
+
+            getRandomColor()
+            {
+                var letters = '0123456789ABCDEF'
+                var color = '#'
+
+                for (var i = 0; i < 6; i++)
+                    color += letters[Math.floor(Math.random() * 16)]
+
+                return color
+            },
         },
+
         watch:
         {
             selected_iso(value)
@@ -67,9 +146,18 @@
                     this.getReports(value)
             }
         },
+
         mounted()
         {
             this.getCountries()
         }
     }
 </script>
+
+
+<style>
+    .small {
+        max-width: 600px;
+        margin:  150px auto;
+    }
+</style>
--- src/services/axios.js
+++ src/services/axios.js
@@ -20,6 +20,17 @@ axios.interceptors.response.use(
                 allowEscapeKey: false,
                 allowOutsideClick: false
             })
+        else if (error.response.status === 422)
+            Swal.fire({
+                title: 'Error',
+                text: error.response.data.message? error.response.data.message : "Given data was invalid",
+                icon: 'warning',
+                showCancelButton: false,
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'OK',
+                allowEscapeKey: false,
+                allowOutsideClick: false
+            })
         return Promise.reject(error)
     }
 )
